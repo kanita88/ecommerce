aeb7d907113ae793d14fe8cd3d0d0770343d32b4
kanita88 <kanitalim88@gmail.com>
2014-12-02 17:40:11 +0100

    a jour

--- application/views/login.php
+++ application/views/login.php
@@ -2,37 +2,66 @@
 <html lang="fr">
 	<head>
 		<meta charset="UTF-8">
-		<title>BLOG</title>
+		<title>E-commerce LEGO</title>
 		<link rel="stylesheet" href="<?=URL::base()?>/assets/css/normalize.css">
 		<link rel="stylesheet" href="<?=URL::base()?>/assets/css/style.css">
-		<link href='http://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Play' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
+		<div class="main">
+			<header>
+				<nav>
+					<div class="img">
+						<img src="<?=URL::base()?>/assets/img/logo-retina.png" alt="logo">
+					</div>
+					<ul class="menu">
+						<li><a href="<?=URL::base()?>">Accueil</a></li>
+						<li><a href="#">Products</a></li>
+						<li><a href="#">Ventes et offres</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</nav>
+			</header>
 
-		<section>
+			<section>
+				<h1>TEST</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod placeat hic illum repellendus debitis delectus eum cum. Sint eos excepturi autem laboriosam, nam fugit quae possimus, in, ut numquam dicta.</p>
+				
+			</section>
 
-			<h1><a href="#">Mon Site Ecommerce</a></h1>
-			
-		<div class="page">
+			<aside>
+			<div class="page">
+				<div class="aside1">
+					<p><a href="<?=URL::base()?>user/login">Se connecter</a> | <a href="<?=URL::base()?>user/register">s'inscrire</a></p>
+				<form action="" method="post">
+						<input type="text" name="emailOrName" placeholder="Email ou Nom" size="20">
+						<br>
+						<input type="password" name="password" placeholder="Votre mot de passe" size="20">
+						<br>
+						<br>
+						<button type="submit">Envoyez</button>
+						<br>
+					</form>
+				</div>
+				<div class="cat">
+					<h2>Catégorie</h2>	
+					<ul class="menu1">
+						<li><a href="#">Rebels</a></li>
+						<li><a href="#">Microfighters</a></li>
+						<li><a href="#">TYC</a></li>
+						<li><a href="#">Clone Wars</a></li>
+						<li><a href="#">Episodes I-VI</a></li>
+						<li><a href="#">Exclusivités</a></li>
+						<li><a href="#">L'ancienne République</a></li>
+					</ul>
+				</div>
 
-			<h1>Veuillez vous connecté à votre session.</h1>
-			<br>
-			<br>
-			<form action="" method="post">
-			<label for="emailOrName">votre Nom ou Addresse email</label><br>
-			<input type="text" name="emailOrName" placeholder="Email or Username" size="35">
-			<br>
-			<label for="password">Mot de passe:</label><br>
-			<input type="password" name="password" placeholder="Votre mot de passe" size="35">
-			<br>
-			<button type="submit">Envoyez</button>
-			</form>
+			</div>
+			</aside>
 		</div>
-		<p>Pas encore de compte ? <a href="<?=URL::base()?>user/register">Inscrivez-vous !</p>
-		</section>
-		
-		<footer>
-			
-		</footer>
+
+			<footer>
+				
+			</footer>
 	</body>
 </html>
--- application/views/register.php
+++ application/views/register.php
@@ -2,43 +2,70 @@
 <html lang="fr">
 	<head>
 		<meta charset="UTF-8">
-		<title>BLOG</title>
+		<title>E-commerce LEGO</title>
 		<link rel="stylesheet" href="<?=URL::base()?>/assets/css/normalize.css">
 		<link rel="stylesheet" href="<?=URL::base()?>/assets/css/style.css">
-		<link href='http://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Play' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
+		<div class="main">
+			<header>
+				<nav>
+					<div class="img">
+						<img src="<?=URL::base()?>/assets/img/logo-retina.png" alt="logo">
+					</div>
+					<ul class="menu">
+						<li><a href="<?=URL::base()?>">Accueil</a></li>
+						<li><a href="#">Products</a></li>
+						<li><a href="#">Ventes et offres</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</nav>
+			</header>
 
-		<section>
+			<section>
+			<h2>Enregistrez vous</h2>
+				<br>
+				<form action="" method="post">
+					<label for="name">Nom: </label><br>
+					<input type="text" name="name" placeholder="Nom" size="35">
+					<br>
+					<label for="firstname">Prénom: </label><br>
+					<input type="text" name="firstname" placeholder="Prénom" size="35">
+					<br>
+					<label for="email">Addresse email</label><br>
+					<input type="text" name="email" placeholder="Votre email" size="35">
+					<br>
+					<label for="password">Mot de passe:</label><br>
+					<input type="password" name="password" placeholder="Votre mot de passe" size="35">
+					<br>
+					<br>
+					<button type="submit">Envoyez</button>
+				</form>
+			</section>
 
-			<h1><a href="#">Mon Site Ecommerce</a></h1>
-			
-		<div class="page">
-
-			<h1>Bienvenue</h1>
-			<h2>Veuillez vous registrer?</h2>
-			<br>
-			<form action="" method="post">
-			<label for="name">Nom: </label><br>
-			<input type="text" name="name" placeholder="Nom" size="35">
-			<br>
-			<label for="firstname">Prénom: </label><br>
-			<input type="text" name="firstname" placeholder="Prénom" size="35">
-			<br>
-			<label for="email">Addresse email</label><br>
-			<input type="text" name="email" placeholder="Votre email" size="35">
-			<br>
-			<label for="password">Mot de passe:</label><br>
-			<input type="password" name="password" placeholder="Votre mot de passe" size="35">
-			<br>
-			<button type="submit">Envoyez</button>
-
-			</form>
+			<aside>
+				<div class="page">
+					<div class="aside1">
+					<p><a href="<?=URL::base()?>user/login">Se connecter</a> | <a href="<?=URL::base()?>user/register">s'inscrire</a></p>
+					<div class="cat">
+						<h2>Catégorie</h2>	
+						<ul class="menu1">
+							<li><a href="#">Rebels</a></li>
+							<li><a href="#">Microfighters</a></li>
+							<li><a href="#">TYC</a></li>
+							<li><a href="#">Clone Wars</a></li>
+							<li><a href="#">Episodes I-VI</a></li>
+							<li><a href="#">Exclusivités</a></li>
+							<li><a href="#">L'ancienne République</a></li>
+						</ul>
+					</div>
+				</div>
+			</aside>
 		</div>
-		</section>
-		
+
 		<footer>
 			
 		</footer>
-	</body>
+</body>
 </html>
